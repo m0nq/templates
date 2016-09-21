cafde753bcefa2a8dc26c34a1b77de118fdc1718
Monk <forgetmenaught@gmail.com>
2016-09-21 12:55:08 -0700

    initial commit: generate angular scaffolding

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name templatesApp
+ * @description
+ * # templatesApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('templatesApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name templatesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the templatesApp
+ */
+angular.module('templatesApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

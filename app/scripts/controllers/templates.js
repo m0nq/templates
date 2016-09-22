d9221ca60bfd2837b315cd121f8773d7b89cff00
Monk <forgetmenaught@gmail.com>
2016-09-22 14:37:55 -0700

    create template details links, controller, and route

--- app/scripts/controllers/templates.js
+++ app/scripts/controllers/templates.js
@@ -15,9 +15,16 @@ angular.module('templatureApp.templates', ['ngRoute'])
   .when('/templates', {
     templateUrl: 'templates/templates.html',
     controller: 'TemplatesCtrl',
-  });
+  })
+	.when('/templates/:templatesId', {
+		templateUrl: 'views/template-details.html',
+		controller: 'TemplateDetailsCtrl'
+	});
 }])
 
 .controller('TemplatesCtrl', ['$scope', function ($scope) {
-  console.log($scope);
+}])
+
+.controller('TemplateDetailsCtrl', ['$scope', function ($scope) {
+
 }]);
